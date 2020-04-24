40608d429f4971764030550097f796b34e6e58ca
rakhin <robina982@gmail.com>
2020-04-23 23:18:29 -0700

    commit on 'master'

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+/* Mods:
+ 1. Create New Artwork for All in-game assets (25)
+ 2. Mouse Control for Player Movement + Firing (25) 
+ 3. Created Own Mod: Asteroid Obstacles (15. Got the idea from the new weapon mod but there no new behaviors so I took 10 points off) 
+ 4. Allow the player to control Rocket after Firing (10) 
+ 5. Created Own Mod: Two Player Simultaneous Co-op (25. Only 25 because I didn't have time to implement the full 50 points version)  */
+
+
 let config = {
     type: Phaser.CANVAS, 
     width: 640,
